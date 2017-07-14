ec0f8a9529bfdcd16a4d77eccc8116f38ad1ff06
Martin Kersner <m.kersner@gmail.com>
2017-07-14 18:49:22 +0900

    train only as autoencoder; not successful

--- generate_sequences.py
+++ generate_sequences.py
@@ -10,14 +10,14 @@ def main():
     arr_len = 6
     start   = 10
     step    = 4
-    end     = 1000
+    end     = 50000
 
     sequences = []
 
     for idx in range(start, end, step):
         sequences.append(Hailstone(idx).generate_sequence()[:arr_len])
 
-    np.save("hailstone_sequences.npy", np.array(sequences))
+    np.save("hailstone_sequences_50000.npy", np.array(sequences))
 
 if __name__ == "__main__":
     main()
